db103bc09ea980d4348f34e353dc466359a54e17
Rahul <your_email@example.com>
2026-03-31 16:52:23 +0530

    Initial commit - Flask Login/register

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_session import Session
+import os
+# os - use to access environment variables like port
+
+app = Flask(__name__)
+app.secret_key = "simple_secret_key"
+
+
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_PERMANENT"] = False
+# Expire when browser closes
+Session(app)
+
+users = {}
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username or not password:
+            return "Username and password required"
+
+        if username in users:
+            return "User already exists"
+
+        users[username] = password
+        return redirect(url_for("login"))
+
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username in users and users[username] == password:
+            session["user"] = username
+            return redirect(url_for("dashboard"))
+        else:
+            return "Invalid username or password"
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user" not in session:
+        return redirect(url_for("login"))
+
+    return render_template("dashboard.html", user=session["user"])
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
